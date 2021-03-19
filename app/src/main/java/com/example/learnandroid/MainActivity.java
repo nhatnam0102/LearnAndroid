cf7a37e6e3b7ef0c0c22e281671025de37f846d0
LearnAdroid <buinhatnam01021997@gmail.com>
2021-03-19 13:40:41 +0900

    test value, implicit,explicit

--- app/src/main/java/com/example/learnandroid/MainActivity.java
+++ app/src/main/java/com/example/learnandroid/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.learnandroid;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button btnClick = findViewById(R.id.btnClick);
+        btnClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i  = new Intent(MainActivity.this, SecondActivity.class);
+                i.putExtra("value1","Lay gia tri value 1 tu secondActivity");
+                i.putExtra("value2","Lay gia tri value3 tu SecondActivity");
+                int REQUEST_CODE=9;
+                startActivityForResult(i,REQUEST_CODE);
+            }
+        });
     }
-}
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode==RESULT_OK&& requestCode==9){
+
+                Toast.makeText(this,data.getExtras().getString("returnValue1"),Toast.LENGTH_LONG).show();
+            Toast.makeText(this,data.getExtras().getString("returnValue2"),Toast.LENGTH_LONG).show();
+            }
+        }
+    }
--- app/src/main/java/com/example/learnandroid/SecondActivity.java
+++ app/src/main/java/com/example/learnandroid/SecondActivity.java
@@ -2,6 +2,7 @@ package com.example.learnandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class SecondActivity extends AppCompatActivity {
@@ -10,5 +11,15 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        finish();
+
+    }
+    @Override
+    public void finish(){
+        Intent data= new Intent();
+        data.putExtra("returnValue1"," value 1 cua SecondActivity");
+        data.putExtra("returnValue2","value2 cua SecondActivity");
+        setResult(RESULT_OK,data);
+        super.finish();
     }
 }
